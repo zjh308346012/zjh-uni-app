064de9d66dd85d0bf0621e998da68c0b1c59ef90
zjh <1127893332@qq.com>
2022-10-29 19:50:29 +0800

    提交也没

--- pages/catr/catr.vue
+++ pages/catr/catr.vue
@@ -1,17 +1,22 @@
 <template>
 <<<<<<< HEAD
+<<<<<<< HEAD
   <view class="">111</view>
 =======
   <view>
     catr
   </view>
 >>>>>>> 257cbe6 (设置了tabar导航以及新增图片)
+=======
+  <view class="">111</view>
+>>>>>>> 56385ea (首页的简单交互完成)
 </template>
 
 <script>
   export default {
     data() {
       return {
+<<<<<<< HEAD
 <<<<<<< HEAD
      
       };
@@ -25,6 +30,16 @@
         
       };
 >>>>>>> 257cbe6 (设置了tabar导航以及新增图片)
+=======
+     
+      };
+    },
+    methods: {
+     
+    },
+    onLoad() {
+   
+>>>>>>> 56385ea (首页的简单交互完成)
     }
   }
 </script>
